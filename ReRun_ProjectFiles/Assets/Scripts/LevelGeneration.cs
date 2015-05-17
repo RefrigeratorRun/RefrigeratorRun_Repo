1aaf0ea65abae7c3e57f1624269edc2cc722f278
bypie5 <bypie966@gmail.com>
2015-05-17 16:23:56 -0700

    @4:20 5/17/2015 ISSUES: none
    Summary:
    - Imported blend files and textures for tiles, fruits, characters, and
    hazards
    - Created a rudimentary system access tile types
    
    Tile type access system:
    - Tiles added as instances in the world
    - 7 cases: 1 = straight... 7 = All 3 turns
    - LOGIC hold an array of all 21 types of tiles
    - Eventually CLASS_TILE will have getter methods to include the
    attachments point of the tile (Two types of attachment, a and b. a is
    the origin while b is where the new tiles will branch off of)

---
+++ ReRun_ProjectFiles/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGeneration : MonoBehaviour {
+
+	public GameObject[] TileMotifs_Objects = new GameObject[21];
+	
+	void Start() {
+		for (int i = 0; i < TileMotifs_Objects.Length; i ++) {
+			print (TileMotifs_Objects[i].name + ", " + TileMotifs_Objects[i].GetComponent<Tile>().GetTurns());
+		}
+	}
+}
---
+++ ReRun_ProjectFiles/Assets/Scripts/Tile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+		
+	//Types 1 : Straight, 2 : Right, 3 : Left, 4 Straight Right, 5 Straight Left, 6 Left Right, 7 All
+	public int Turns;
+	
+	public int GetTurns () {
+		return Turns;
+	}
+}
